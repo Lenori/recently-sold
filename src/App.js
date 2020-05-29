334cede661f94fb5a27ffffe31853f39c8d84b24
Leo Lenori <leolenori@gmail.com>
2020-05-28 22:26:00 -0300

    updates 2020-05-28

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
+import {ToastContainer} from 'react-toastify';
+
 function App() {
   return(
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
+      <ToastContainer autoClose={2500} />
     </Router>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
 
+import Route from './Route';
+
+import Home from '../pages/Home';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
-import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 
 export default function Routes() {
     return(
         <Switch>
-            <Route path="/" exact component={Signin} />
+            <Route path="/" exact component={Home} />
+            <Route path="/login" component={Signin} />
             <Route path="/register" component={Signup} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/profile" component={Profile} />
+
+            <Route path="/profile" isPrivate component={Profile} />
         </Switch>
     )
 }
